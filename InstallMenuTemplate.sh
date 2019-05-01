da99cf4d78ef340a6cbc77e6ee2b09a904c07ef7
gismarb <gismar_barbosa@yahoo.com.br>
2019-05-01 14:56:12 -0300

    Criando um Instalador simples para Linux (shell script)

---
+++ InstallMenuTemplate.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+# Criado em 01/05/2019
+
+nome_programa="Menu"
+
+title 
+clear
+
+echo
+echo "  --------------- Instalador $nome_programa ---------------"
+echo
+
+date +%H:%M:%S
+
+echo "  --> Iniciando a Instalacao do $nome_programa, aguarde..."
+echo
+
+sleep 10
+g++ main.cpp MenuTemplate/MenuTemplate.cpp MenuTemplate/Entry.cpp MenuTemplate/ExternFunctions/getChar.cpp -o Menu 2> null
+
+date +%H:%M:%S
+
+echo "  --> Finalizando a Instalacao. Sera criado o arquivo $nome_programa em: "
+pwd
+
+sleep 10
+echo
+date +%H:%M:%S
+
+echo "  --> Para utilizar, inicialmente digite ./$nome_programa"
+echo
+#cleaning trash
+
+rm null
+read -p "Pressione qualquer tecla para sair do Instalador..."
+echo
+
+sleep 5
+
